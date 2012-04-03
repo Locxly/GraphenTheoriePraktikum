84ed39d672bbcc8bcb585e5c8aa12c3ec3bb1cce
Stephan Hoelscher <stephan.hoelscher@haw-hamburg.de>
2012-04-03 09:23:45 +0200

    This is the second commit to some stuff.

--- src/gka/GKAHelloWorld.java
+++ src/gka/GKAHelloWorld.java
@@ -13,7 +13,7 @@ public class GKAHelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+		System.out.print("Hello World! This is a test!");
 	}
 
 }
