0e509a3ca38ff679e99daa8d9770a713ad5d38d0
Stephan Hoelscher <stephan.hoelscher@haw-hamburg.de>
2012-06-06 12:13:53 +0200

    another try

--- src/gka/NodeType.java
+++ src/gka/NodeType.java
@@ -10,17 +10,57 @@ class NodeType<V, E>
     /**
 	 * 
 	 */
-	private EdmondsKarpFlowAlgorithm<V, E> NodeType;
-	V prototype; // corresponding node in the original network
-    List<EdgeType> outgoingEdges = new ArrayList<EdgeType>(); // list of outgoing arcs
+	private V prototype; // corresponding node in the original network
+    private List<EdgeType> outgoingEdges = new ArrayList<EdgeType>(); // list of outgoing arcs
                                                    // in the residual
                                                    // network
-    boolean visited; // this mark is used during BFS to mark visited nodes
-    EdgeType lastEdge; // last arc in the shortest path
-    double flowAmount; // amount of flow, we are able to push here
+    private boolean visited; // this mark is used during BFS to mark visited nodes
+    private EdgeType lastEdge; // last arc in the shortest path
+    private double flowAmount; // amount of flow, we are able to push here
 
-    NodeType(EdmondsKarpFlowAlgorithm<V, E> edmondsKarpMaximumFlow, V prototype) {
-        NodeType = edmondsKarpMaximumFlow;
+    NodeType(V prototype) {
 		this.prototype = prototype;
     }
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setLastEdge(EdgeType lastEdge) {
+		this.lastEdge = lastEdge;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public double getFlowAmount() {
+		return flowAmount;
+	}
+
+	public void setFlowAmount(double flowAmount) {
+		this.flowAmount = flowAmount;
+	}
+
+	public V getPrototype() {
+		return prototype;
+	}
+
+	public List<EdgeType> getOutgoingEdges() {
+		return outgoingEdges;
+	}
+
+	public EdgeType getLastEdge() {
+		return lastEdge;
+	}
+	
+	public void addOutgoingEdge(EdgeType e){
+		outgoingEdges.add(e);
+	}
+	
+	public void mark(EdgeType last, double posFlow)
+	{
+		lastEdge = last;
+		flowAmount = posFlow;
+	}
 }
